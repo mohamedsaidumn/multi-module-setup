6dcb4f4343ef6cf69d52535b4eac0e918e6a4104
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-02-09 16:41:11 -0600

    set up for multi module java project

---
+++ module-1/src/main/java/com/said/DateUtils.java
@@ -0,0 +1,8 @@
+package com.said;
+
+public class DateUtils {
+
+    public static String getDate(){
+        return "2/9/2023";
+    }
+}
---
+++ module-2/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import com.said.DateUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(DateUtils.getDate());
+    }
+}
